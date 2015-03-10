8d53b8bc11fbe2550f65274e41081bd859fab390
hansenling <hansenling@berkeley.edu>
2015-03-09 23:25:57 -0700

    Revert "Revert "First Commit""
    This reverts commit efb30ab237a63232d28b2edf4c9504e1c90eeade.

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour {
+	public int displayScore = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (displayScore);
+		this.guiText.text = "Score: " + displayScore;
+	}
+}
+
---
+++ Assets/Scripts/shoot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+	public GameObject bullet;
+	public double myTimerStart = 2.0f;
+	static double myTimer;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTimer -= Time.deltaTime;
+		if (myTimer < 0.0f) {
+			bullet.GetComponent<startBullet>().direction = GameObject.FindGameObjectWithTag("MainCamera");
+			bullet.GetComponent<startBullet>().lifeTime = 10.0f;
+			Instantiate (bullet, this.transform.position, Quaternion.identity);
+
+			myTimer = myTimerStart;
+
+		}
+	}
+}
